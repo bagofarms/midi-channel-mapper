d71dcba0b681afba4984d24443fbb4c5c6e35c4e
Zachary Berry <zachberry@gmail.com>
2018-05-13 10:57:47 -0400

    * Adds a few comments * Implement handlers (except sysex) * Implement display routines for all non-realtime handlers

--- arduino_midi_oled.ino
+++ arduino_midi_oled.ino
@@ -38,9 +38,26 @@ byte prog_pass_channel = 14; // Channel to pass through program change events
 void setup() {
   MIDI.setHandleNoteOn(handleNoteOn);
   MIDI.setHandleNoteOff(handleNoteOff);
+
+  MIDI.setHandleControlChange(handleControlChange);
   MIDI.setHandleProgramChange(handleProgramChange);
+  MIDI.setHandleAfterTouchPoly(handleAfterTouchPolyPressure);
+  MIDI.setHandleAfterTouchChannel(handleAfterTouchChannelPressure);
   MIDI.setHandlePitchBend(handlePitchBend);
+  MIDI.setHandleTimeCodeQuarterFrame(handleTimeCodeQuarterFrame);
+  MIDI.setHandleSongPosition(handleSongPosition);
+  MIDI.setHandleSongSelect(handleSongSelect);
+  MIDI.setHandleTuneRequest(handleTuneRequest);
+  MIDI.setHandleClock(handleClock);
+  MIDI.setHandleStart(handleStart);
+  MIDI.setHandleContinue(handleContinue);
+  MIDI.setHandleStop(handleStop);
+  MIDI.setHandleActiveSensing(handleActiveSensing);
+  MIDI.setHandleSystemReset(handleSystemReset);
+  // (SysEx messages are not supported)
+
   MIDI.begin(MIDI_CHANNEL_OMNI);
+
   MIDI.turnThruOff();
 
   // OLED Display
@@ -51,36 +68,32 @@ void setup() {
 }
 
 void loop() {
-  // Write values to screen
-  //writeOLED(chanIn, chanOut);
   MIDI.read();
 }
+
+byte getOutChannel() {
+  // TODO: Switch between the dynamic and manual override channel
+  return dyn_channel;
+}
+
+// -------------------------------------------
+// MIDI Handlers:
+// -------------------------------------------
 
 void handleProgramChange(byte channel, byte number) {
   // If we're on the input channel
   if(channel == in_channel) {
-    // Split off the first byte to find output channel to change to
     dyn_channel = number;
     dyn_channel += 1; // Adding 1 because the midi library is 1-indexed
-    
-    // Remove the first byte to send out
-    // byte new_num = number & 0x0F;
     displayChannels();
   } else if(channel == prog_pass_channel) {
     MIDI.sendProgramChange(number, prog_pass_channel);
   }
 }
 
-void handleAftertouch(byte command, byte data1) {
-  // Don't do anything here because aftertouch sends data too fast
-  // and it will bog down the serial reads.
-  Serial.write(command);
-  Serial.write(data1);
-}
-
 void handleNoteOn(byte channel, byte note, byte velocity) {
   if(channel == in_channel) {
-    if(DEBUG){ displayNoteOLED(channel, note, velocity); }
+    if(DEBUG) { displayNoteOLED(channel, note, velocity); }
     MIDI.sendNoteOn(note, velocity, getOutChannel());
   } else if(channel == prog_pass_channel) {
     MIDI.sendNoteOn(note, velocity, prog_pass_channel);
@@ -89,7 +102,7 @@ void handleNoteOn(byte channel, byte note, byte velocity) {
 
 void handleNoteOff(byte channel, byte note, byte velocity) {
   if(channel == in_channel) {
-    if(DEBUG){ displayNoteOLED(channel, note, velocity); }
+    if(DEBUG) { displayNoteOLED(channel, note, velocity); }
     MIDI.sendNoteOff(note, velocity, getOutChannel());
   } else if(channel == prog_pass_channel) {
     MIDI.sendNoteOff(note, velocity, prog_pass_channel);
@@ -98,34 +111,102 @@ void handleNoteOff(byte channel, byte note, byte velocity) {
 
 void handlePitchBend(byte channel, int bend) {
   if(channel == in_channel) {
-    if(DEBUG){ displayPitchBend(channel, bend); }
+    if(DEBUG) { displayPitchBend(bend); }
     MIDI.sendPitchBend(bend, getOutChannel());
   } else if(channel == prog_pass_channel) {
     MIDI.sendPitchBend(bend, prog_pass_channel);
   }
 }
 
-byte getOutChannel() {
-  // TODO: Switch between the dynamic and manual override channel
-  return dyn_channel;
+void handleControlChange(byte channel, byte number, byte value) {
+  if(channel == in_channel) {
+    if(DEBUG) { displayControlChange(number, value); }
+    MIDI.sendControlChange(number, value, getOutChannel());
+  } else if(channel == prog_pass_channel) {
+    MIDI.sendControlChange(number, value, prog_pass_channel);
+  }
 }
 
-// Display
+void handleAfterTouchPolyPressure(byte channel, byte note, byte pressure) {
+  if(channel == in_channel) {
+    if(DEBUG) { displayAfterTouchPolyPressure(note, pressure); }
+    MIDI.sendAfterTouch(note, pressure, getOutChannel());
+  } else if(channel == prog_pass_channel) {
+    MIDI.sendAfterTouch(note, pressure, prog_pass_channel);
+  }
+}
+
+void handleAfterTouchChannelPressure(byte channel, byte pressure) {
+  if(channel == in_channel) {
+    if(DEBUG) { displayAfterTouchChannelPressure(pressure); }
+    MIDI.sendAfterTouch(pressure, getOutChannel());
+  } else if(channel == prog_pass_channel) {
+    MIDI.sendAfterTouch(pressure, prog_pass_channel);
+  }
+}
+
+void handleTimeCodeQuarterFrame(byte data) {
+  MIDI.sendTimeCodeQuarterFrame(data);
+}
+
+void handleSongPosition(unsigned beats) {
+  MIDI.sendSongPosition(beats);
+}
+
+void handleSongSelect(byte songnumber) {
+  MIDI.sendSongSelect(songnumber);
+}
+
+void handleTuneRequest() {
+  MIDI.sendTuneRequest();
+}
+
+// Real time messages (Messages that don't specify channels)
+// (These messages don't have their own sendX method, instead we must use a
+// lower-level send method from the library):
+void handleClock() {
+  MIDI.sendRealTime(MIDI::Clock);
+}
+
+void handleStart() {
+  MIDI.sendRealTime(MIDI::Start);
+}
+
+void handleContinue() {
+  MIDI.sendRealTime(MIDI::Continue);
+}
+
+void handleStop() {
+  MIDI.sendRealTime(MIDI::Stop);
+}
+
+void handleActiveSensing() {
+  MIDI.sendRealTime(MIDI::ActiveSensing);
+}
+
+void handleSystemReset() {
+  MIDI.sendRealTime(MIDI::SystemReset);
+}
+
+// -------------------------------------------
+// Display methods:
+// -------------------------------------------
+
 void displayChannels(){
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
   display.print(OLED_HEADER);
-  
+
   display.setTextSize(5);
-  
+
   display.setCursor(0,SIZE5_2DIGIT_YOFFSET);
   oledPrintWithLeadingZero(in_channel);
 
   display.setCursor(SIZE5_2DIGIT_XOFFSET,SIZE5_2DIGIT_YOFFSET);
   oledPrintWithLeadingZero(dyn_channel);
-  
+
   display.display();
 }
 
@@ -143,13 +224,45 @@ void displayNoteOLED(byte channel, byte note, byte velocity) {
   display.display();
 }
 
-void displayPitchBend(byte channel, int bend) {
+void displayPitchBend(int bend) {
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
   display.println("Pitch Bend");
   display.println(bend);
+  display.display();
+}
+
+void displayControlChange(byte number, byte value) {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("CC");
+  display.println(number);
+  display.println(value);
+  display.display();
+}
+
+void displayAfterTouchChannelPressure(byte pressure) {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("AftrTouchChan");
+  display.println(pressure);
+  display.display();
+}
+
+void displayAfterTouchPolyPressure(byte note, byte pressure) {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("AftrTouchPoly");
+  display.println(note);
+  display.println(pressure);
   display.display();
 }
 
