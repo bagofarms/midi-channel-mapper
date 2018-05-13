985776e0ba63a63ce2968f110c74f66031795366
Jacob Bates <jacob.bates@gmail.com>
2018-05-12 20:27:10 -0400

    Converted to using midi library)

--- arduino_midi_oled.ino
+++ arduino_midi_oled.ino
@@ -1,3 +1,5 @@
+#include <MIDI.h>
+
 #include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
 
 #include <Adafruit_SSD1306.h>
@@ -24,17 +26,13 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define CHAN_IN_POT 2
 #define CHAN_OUT_POT 3
 
-// MIDI Constants
-#define COMMAND_PROGRAMCHANGE 0xC
-#define COMMAND_AFTERTOUCH 0xD
-#define MASK_CHANNEL 0x0F
-
-int command;
-int data1;
-int data2;
+MIDI_CREATE_DEFAULT_INSTANCE();
 
 void setup() {
-  Serial.begin(31250);
+  MIDI.setHandleNoteOn(handleNoteOn);
+  MIDI.setHandleNoteOff(handleNoteOff);
+  MIDI.setHandleProgramChange(handleProgramChange);
+  MIDI.begin(MIDI_CHANNEL_OMNI);
 
   // OLED Display
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -44,80 +42,49 @@ void setup() {
 void loop() {
   // Write values to screen
   //writeOLED(chanIn, chanOut);
-  if (Serial.available() > 2){
-    command = Serial.read();//read first byte
-    data1 = Serial.read();//read next byte
-
-    // Error handling
-    if(command < 127){
-      command = 127;
-    }
-
-    if(data1 < 0){
-      data1 = 0;
-    }
-
-    // Split the command byte
-    int op = (command >> 4);
-
-    switch(op) {
-      case COMMAND_PROGRAMCHANGE:
-        data2 = 0;
-        handleProgramChange(command, data1);
-        break;
-
-      case COMMAND_AFTERTOUCH:
-        // if it's aftertouch (0xD), so don't read the second data byte
-        data2 = 0;
-        handleAftertouch(command, data1);
-        // Don't do anything else in the loop when aftertouch is happening
-        break;
-
-      default:
-        // Since it's not a special case, read the second data byte
-        data2 = Serial.read();//read final byte
-      
-        if(data2 < 0){
-          data2 = 0;
-        }
-        
-        handleMidi(command, data1, data2);
-        // Do extra stuff here if you want to
-    }
-  }
+  MIDI.read();
 }
 
-void handleProgramChange(int command, int data1) {
+void handleProgramChange(byte channel, byte number) {
   // Do something special here like changing our output channel?
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
   display.println("ProgramChg");
-  display.println(command);
-  display.println(data1);
+  display.println(channel);
+  display.println(number);
   display.display();
-  Serial.write(command);
-  Serial.write(data1);
+  MIDI.sendProgramChange(channel, number);
 }
 
-void handleAftertouch(int command, int data1) {
+void handleAftertouch(byte command, byte data1) {
   // Don't do anything here because aftertouch sends data too fast
   // and it will bog down the serial reads.
   Serial.write(command);
   Serial.write(data1);
 }
 
-void handleMidi(int command, int data1, int data2) {
+void handleNoteOn(byte channel, byte note, byte velocity) {
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.println(command);
-  display.println(data1);
-  display.println(data2);
+  display.println(channel);
+  display.println(note);
+  display.println(velocity);
   display.display();
-  Serial.write(command);
-  Serial.write(data1);
-  Serial.write(data2);
+  MIDI.sendNoteOn(channel, note, velocity);
+}
+
+void handleNoteOff(byte channel, byte note, byte velocity) {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println(channel);
+  display.println(note);
+  display.println(velocity);
+  display.display();
+  MIDI.sendNoteOff(channel, note, velocity);
 }
